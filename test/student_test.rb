43900d823c3a247f32a263aa6e8a91739cfcb9d3
Ezekiel Clark <clarkzeke@gmail.com>
2020-01-10 08:50:50 -0700

    Add test for log score

--- test/student_test.rb
+++ test/student_test.rb
@@ -21,4 +21,11 @@ class StudentTest < MiniTest::Test
   def test_student_score_starts_as_an_empty_array
     assert_equal [], @student.scores
   end
+
+  def test_log_score_adds_to_scores_attribute
+    @student.log_score(89)
+    @student.log_score(78)
+
+    assert_equal [89,78], @student.scores
+  end
 end
