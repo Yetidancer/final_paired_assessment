a01e3b10247e037e3385d24e735de22852f47736
Ezekiel Clark <clarkzeke@gmail.com>
2020-01-10 08:44:59 -0700

    Add student attribute test

--- test/student_test.rb
+++ test/student_test.rb
@@ -4,12 +4,19 @@ require 'minitest/pride'
 
 class StudentTest < MiniTest::Test
 
-  def test_it_exists
-    student = Student.new({name: "Morgan", age: 21})
+  def setup
+    @student = Student.new({name: "Morgan", age: 21})
+  end
 
+  def test_it_exists
     assert_instance_of Student, student
   end
 
+  def test_it_has_attributes
+    assert_equal "Morgan", @student.name
+
+    assert_equal 21, @student.age
+  end
 
 
 end
